9e14a4b6e5436db0e54f7dfc26c55f66664194ee
bobsharon <bobsharon@163.com>
2019-08-28 17:53:24 +0800

    demo 0.1.0

--- src/main/java/com/mfksoft/demo/controller/WelcomeController.java
+++ src/main/java/com/mfksoft/demo/controller/WelcomeController.java
@@ -1,10 +1,8 @@
 package com.mfksoft.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
